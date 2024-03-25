dabe46e800407e3fe732ef3773decfca5b5afeea
MuhammadFarhanAlAnzhari <100020637+MuhammadFarhanAlAnzhari@users.noreply.github.com>
2024-03-25 13:21:26 +0700

    Initial commit

---
+++ app/src/main/java/com/zhari/qolami_app/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.zhari.qolami_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zhari.qolami_app.ui.theme.QolamiappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QolamiappTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QolamiappTheme {
+        Greeting("Android")
+    }
+}
